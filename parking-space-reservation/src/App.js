39d1642ff902c479f7e3e584194a69fbefb7755d
Gabriel Quirschfeld <quirschfeld@gmail.com>
2019-08-08 10:00:29 +0200

    blank components

--- parking-space-reservation/src/App.js
+++ parking-space-reservation/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table';
+import Calendar from './components/Calendar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+			<div className="App-header"><h1 className="App-title">Parking Space Reservation System</h1></div>
+			<div className="App-content">
+        <div className="App-table">
+          <Table>
+            
+          </Table>
+        </div>
+        <div className="App-calendar">
+          <Calendar>
+
+          </Calendar>
+        </div>
+			</div>
+		</div>
   );
 }
 
